21b59fc524c933227e1103ce004549c10197421b
iks041 <ishankumarsah400@gmail.com>
2024-10-02 20:42:49 +0530

    oh my dog

---
+++ activity-4.py
@@ -0,0 +1,15 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("FirstName :", first_name)
+print("Last Name :", last_name)
+print("Full Name:", full_name)
+print("String Multiplied 5 times gives this result :", example)
+
+word = 'Coding'
+print("Length of string :", len(word))
+print("First letter of string :", word[0])
+print("Last leter of string :", word[5])
+print("String sliced :", word[0:3])
